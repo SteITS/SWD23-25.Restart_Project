72b8d045fd06f8313292afcd40d95fe5e083c711
Giordano Bruno Michela <giordano.michela@edu.itspiemonte.it>
2024-10-02 12:01:24 +0200

    fix - convertiti id di User e Role da Long a int per evitare errori in avvio di Spring boot (nonostante si avviasse comunque)

--- MegaDex/src/main/java/com/restart/dto/UserDto.java
+++ MegaDex/src/main/java/com/restart/dto/UserDto.java
@@ -14,7 +14,7 @@ import javax.validation.constraints.NotEmpty;
 @AllArgsConstructor
 public class UserDto
 {
-    private Long id;
+    private int id;
     @NotEmpty
     private String firstName;
     @NotEmpty
--- MegaDex/src/main/java/com/restart/entity/User.java
+++ MegaDex/src/main/java/com/restart/entity/User.java
@@ -21,7 +21,7 @@ public class User
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private int id;
 
     @Column(nullable=false)
     private String name;
