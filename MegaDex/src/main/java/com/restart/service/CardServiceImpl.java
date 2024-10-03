c8084d3197b100707ed776600dbf90f46a0a4c2b
SteITS <stefan.stanescu@edu.itspiemonte.it>
2024-10-03 18:26:22 +0200

    Aggiune CardService,CardServiceImpl e CardDAO con funzionalità findAll

---
+++ MegaDex/src/main/java/com/restart/service/CardServiceImpl.java
@@ -0,0 +1,24 @@
+package com.restart.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.restart.entity.Card;
+import com.restart.repository.CardDAO;
+@Service
+public class CardServiceImpl implements CardService{
+
+	@Autowired
+	private CardDAO dao;
+	
+	
+
+	@Override
+	public List<Card> getCards() {
+		return dao.findAll();
+	}
+
+	
+}
