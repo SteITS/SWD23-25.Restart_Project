9765a7e05ff29c4ae4804af0af3cfa6c5a53bf0c
SteITS <stefan.stanescu@edu.itspiemonte.it>
2024-10-03 18:42:14 +0200

    fixato import di Card e CardService in CardRest

--- MegaDex/src/main/java/com/restart/controller/CardRest.java
+++ MegaDex/src/main/java/com/restart/controller/CardRest.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.restart.entities.Card;
-import com.restart.services.CardService;
+import com.restart.entity.Card;
+import com.restart.service.CardService;
 
 
 @RestController
